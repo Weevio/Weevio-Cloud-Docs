a396376fe95cfca0cd515bd15b10e5055b6a8e27
Andrew Kirchofer <andrewkirchofer@gmail.com>
2025-10-15 17:36:49 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <section className="flex min-h-[86.5vh] flex-col items-center justify-center px-2 py-8 text-center">
             <h1 className="mb-4 text-4xl font-bold sm:text-7xl">Weevio Cloud</h1>
             <p className="text-foreground mb-8 max-w-[800px] sm:text-lg">
-                PLACEHOLDER
+                PLACEHOLDER SUBTITLE
             </p>
 
             <div className="flex items-center gap-5">
